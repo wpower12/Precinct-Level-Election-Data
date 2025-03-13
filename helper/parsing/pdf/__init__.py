d35d7d8f4bc6c738e5376394adac94c358581c71
wpower12 <willpowe@gmail.com>
2025-03-13 15:33:43 -0400

    cleaning up helper module.

--- helper/parsing/pdf/__init__.py
+++ helper/parsing/pdf/__init__.py
@@ -19,15 +19,36 @@ class PrecinctReturn(BaseModel):
             'race':      self.race,
             'party':     self.party,
             'candidate': self.candidate,
-            'votes':  self.votes,
+            'votes':     self.votes,
         }
 
 class Returns(BaseModel):
     returns: list[PrecinctReturn]
 
 
-DEFAULT_SPPP_PROMPT = """
+DEFAULT_SPPP_PROMPT = """You will be given the text pages of a PDF that contain tabular information about election returns. 
+Your task will be to parse the information into a list of returns. Each entry in the list will represent a single candidates
+total votes from a specific precinct, in a specific race. 
 
+You will be sent multiple pages. Each page will have a consistent layout.
+The page you are shown will contain a section at the top, a header, which contains the election name and date, the county name, and the precinct name.
+After the header, there will be multiple tabular sections, each containing a contiguous set of rows of data for a specific race. 
+The name of the race (<RACE NAME>) will be written before the block of tabular data.
+
+For each of the rows of data that contain a candidate (not the aggregated data in the last two rows), you will create a new item for the result list you will return to me. 
+Each row in the table contains data in the following format: <PARTY> <NAME> <TOTAL VOTES> <ELECTION DAY VOTES> <MAIL IN VOTES> <PROVISIONAL BALLOTS>
+
+For each of the races in the text of the PDF document, you will create a new piece of data. This data will look like this:
+{
+    'precinct': <PRECINCT NAME>,
+    'year': <ELECTION YEAR>,
+    'race': <RACE NAME>,
+    'party': <PARTY>,
+    'candidate': <NAME>,
+    'votes': <TOTAL VOTES>
+}
+
+Your task is to return the full list of election return data from the set of pages you are sent. 
 """
 
 
@@ -38,7 +59,7 @@ def make_batches(n, b):
 
 def parse_simple_precinct_per_page_llm(path_pdf, oai_client, model_str, batch_size, prompt=DEFAULT_SPPP_PROMPT, debug=False):
     # I'm worried about keeping a fp open the entire time we're waiting for the model to return a result, so
-    # i'm gonna be cautious, and open the file, get what I need, and close it. So things will be redundant occasionally
+    # I'm gonna be cautious, and open the file, get what I need, and close it. So things will be redundant occasionally
     with open(path_pdf, 'rb') as f:
         num_pages = len(PdfReader(f).pages)
 
